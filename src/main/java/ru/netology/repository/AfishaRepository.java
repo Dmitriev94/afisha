0c986e2777f266b6fecb7ec66fcdf5c6af3273c2
Ivan Dmitriev <dmitrievstekloy@gmail.com>
2020-06-13 14:49:48 +0300

    findById updated

--- src/main/java/ru/netology/repository/AfishaRepository.java
+++ src/main/java/ru/netology/repository/AfishaRepository.java
@@ -4,56 +4,57 @@ import ru.netology.domain.Movie;
 
 public class AfishaRepository {
 
-        private Movie[] movies = new Movie[0];
+    private Movie[] movies = new Movie[0];
 
-        public Movie[] findAll() {
-            return movies;
-        }
+    public Movie[] findAll() {
 
-        public void save(Movie movie) {
-            int length = movies.length + 1;
-            Movie[] watchLater = new Movie[length];
-
-            System.arraycopy(movies, 0, watchLater, 0, movies.length);
-            int lastIndex = movies.length - 1;
-            watchLater[lastIndex] = movie;
-
-            movies = watchLater;
-        }
-
-        // public Movie[] findById(int id) {
-
-        //   Movie[] watchLater = new Movie[movies.length];
-        // int index = 0;
-        // for (Movie item : movies) {
-        //   if (item.getId() == id) {
-        //     watchLater[index] = item;
-
-
-        //  }
-        // }
-        // }
-
-        public void removeById(int id) {
-            int length = movies.length - 1;
-            Movie[] watchLater = new Movie[length];
-            int index = 0;
-            for (Movie item : movies) {
-                if (item.getId() != id) {
-                    watchLater[index] = item;
-                    index++;
-                }
-            }
-
-            movies = watchLater;
-        }
-
-        public Movie[] removeAll() {
-            movies = new Movie[0];
-            return movies;
-        }
-
+        return movies;
     }
 
+    public void save(Movie movie) {
+        int length = movies.length + 1;
+        Movie[] watchLater = new Movie[length];
+
+        System.arraycopy(movies, 0, watchLater, 0, movies.length);
+        int lastIndex = movies.length - 1;
+        watchLater[lastIndex] = movie;
+
+        movies = watchLater;
+    }
+
+    public Movie[] findById(int id) {
+
+        Movie[] watchLater = new Movie[movies.length];
+
+        for (Movie item : movies) {
+            if (item.getId() == id) {
+
+                return new Movie[]{item};
+            }
+            {
+                return null;
+            }
 
 
+            public void removeById( int id){
+                int length = movies.length - 1;
+                Movie[] watchLater = new Movie[length];
+                int index = 0;
+                for (Movie item : movies) {
+                    if (item.getId() != id) {
+                        watchLater[index] = item;
+                        index++;
+                    }
+                }
+
+                movies = watchLater;
+            }
+
+
+            public Movie[] removeAll() {
+                movies = new Movie[0];
+                return movies;
+            }
+
+
+        }
